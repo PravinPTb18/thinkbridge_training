f081f40ebc5173b26126c8942e839825e86b026b
pravin18041994 <pravinpalkonda18@gmail.com>
2022-06-14 14:10:42 +0530

    adding files to new branch

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
+      // debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primarySwatch: context.watch<ThemeProvider>().mainColor,
           brightness: context.watch<ThemeProvider>().brightness,
